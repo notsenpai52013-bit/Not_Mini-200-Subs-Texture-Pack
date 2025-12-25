644d8a22b0e7ed55bef95aef9c40c3c00a439d98
notsenpai52013-bit <notsenpai52013@gmail.com>
2025-12-25 14:39:44 +0500

    Update background.js

--- background.js
+++ background.js
@@ -1,30 +1,5 @@
 const URL_MAP = {
-  "||miniblox.*textures/spritesheet.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/refs/heads/main/spritesheet-t%20(6)%20(7)%20(1).png",
-  "||miniblox.*textures/items/diamond_axe.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/diamond_axe.png",
-  "||miniblox.*textures/items/diamond_hoe.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/diamond_hoe.png",
-  "||miniblox.*textures/items/diamond_pickaxe.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/diamond_pickaxe.png",
-  "||miniblox.*textures/items/diamond_shovel.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/diamond_shovel.png",
-  "||miniblox.*textures/items/diamond_sword.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/diamond_sword.png",
-  "||miniblox.*textures/items/gold_axe.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/gold_axe.png",
-  "||miniblox.*textures/items/gold_hoe.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/gold_hoe.png",
-  "||miniblox.*textures/items/gold_pickaxe.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/gold_pickaxe.png",
-  "||miniblox.*textures/items/gold_shovel.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/gold_shovel.png",
-  "||miniblox.*textures/items/gold_sword.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/gold_sword.png",
-  "||miniblox.*textures/items/iron_axe.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/iron_axe.png",
-  "||miniblox.*textures/items/iron_hoe.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/iron_hoe.png",
-  "||miniblox.*textures/items/iron_pickaxe.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/iron_pickaxe.png",
-  "||miniblox.*textures/items/iron_shovel.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/iron_shovel.png",
-  "||miniblox.*textures/items/iron_sword.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/iron_sword.png",
-  "||miniblox.*textures/items/stone_axe.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/stone_axe.png",
-  "||miniblox.*textures/items/stone_hoe.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/stone_hoe.png",
-  "||miniblox.*textures/items/stone_pickaxe.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/stone_pickaxe.png",
-  "||miniblox.*textures/items/stone_shovel.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/stone_shovel.png",
-  "||miniblox.*textures/items/stone_sword.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/stone_sword.png",
-  "||miniblox.*textures/items/wood_axe.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/wood_axe.png",
-  "||miniblox.*textures/items/wood_hoe.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/wood_hoe.png",
-  "||miniblox.*textures/items/wood_pickaxe.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/wood_pickaxe.png",
-  "||miniblox.*textures/items/wood_shovel.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/wood_shovel.png",
-  "||miniblox.*textures/items/wood_sword.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/Tools/wood_sword.png"
+  "||miniblox.*textures/spritesheet.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/refs/heads/main/spritesheet-t%20(6)%20(7)%20(1).png"
 };
 
 let rules = [];
